44db7c2dfcc7472f52c792469d0c76439c2c7025
hardiyan21 <hardiyankesumar@outlook.com>
2019-08-10 14:10:42 +0700

    Add Grid and CardView

--- app/src/main/java/com/dicoding/picodiploma/myrecyclerview/MainActivity.java
+++ app/src/main/java/com/dicoding/picodiploma/myrecyclerview/MainActivity.java
@@ -1,11 +1,16 @@
 package com.dicoding.picodiploma.myrecyclerview;
 
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-
+import com.dicoding.picodiploma.myrecyclerview.adapter.CardViewerHeroAdapter;
+import com.dicoding.picodiploma.myrecyclerview.adapter.GridHeroAdapter;
 import com.dicoding.picodiploma.myrecyclerview.adapter.ListHeroAdapter;
 import com.dicoding.picodiploma.myrecyclerview.model.Hero;
 import com.dicoding.picodiploma.myrecyclerview.model.HeroesData;
@@ -28,9 +33,58 @@ public class MainActivity extends AppCompatActivity {
         showRecyclerList();
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String title = null;
+        switch (item.getItemId()){
+            case R.id.action_list:
+                title = "Mode List";
+                showRecyclerList();
+                break;
+            case R.id.action_grid:
+                showRecyclerGrid();
+                title = "Mode Grid";
+                break;
+
+            case R.id.action_cardview:
+                title = "Mode CardView";
+                showRecyclerCardView();
+                break;
+        }
+        setActionBarTitle(title);
+        return super.onOptionsItemSelected(item);
+    }
+
+
     private void showRecyclerList(){
         rvHeroes.setLayoutManager(new LinearLayoutManager(this));
         ListHeroAdapter listHeroAdapter = new ListHeroAdapter(list);
         rvHeroes.setAdapter(listHeroAdapter);
     }
+
+    private void setActionBarTitle(String title) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
+        }
+    }
+
+    private void showRecyclerGrid(){
+        rvHeroes.setLayoutManager(new GridLayoutManager(this, 2));
+        GridHeroAdapter gridHeroAdapter = new GridHeroAdapter(list);
+        rvHeroes.setAdapter(gridHeroAdapter);
+    }
+
+    private void showRecyclerCardView(){
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        CardViewerHeroAdapter cardViewerHeroAdapter = new CardViewerHeroAdapter(list);
+        rvHeroes.setAdapter(cardViewerHeroAdapter);
+    }
+
 }
