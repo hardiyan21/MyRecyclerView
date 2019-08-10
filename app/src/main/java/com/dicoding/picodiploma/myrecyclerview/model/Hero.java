0c22397cfeba540b38a0f6101cef28d16ab6b4de
hardiyan21 <hardiyankesumar@outlook.com>
2019-08-10 10:42:33 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/picodiploma/myrecyclerview/model/Hero.java
@@ -0,0 +1,32 @@
+package com.dicoding.picodiploma.myrecyclerview.model;
+
+public class Hero {
+    private String name;
+    private String from;
+    private String photo;
+
+    public String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+}
